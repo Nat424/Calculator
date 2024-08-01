8e9a62ac99a65e497c47c3dac7c589f22b213060
Nat424 <n4248386@gmail.com>
2024-08-01 19:29:54 +0400

    Update Calculator.py
    1

--- Calculator.py
+++ Calculator.py
@@ -1,28 +1,28 @@
 str = input()
+def main(str):
+    try:
+        arr = str.split()
+        num1 = int(arr[0])
+        num2 = int(arr[2])
 
-try:
-    arr = str.split()
-    num1 = int(arr[0])
-    num2 = int(arr[2])
+        if (num1 < 0 or num1 > 10):
+            raise Exception("Первое число не верно")
+        if (num2 < 0 or num2 > 10):
+            raise Exception("Второе число не верно")
+        if (arr[1] != '+' and arr[1] != '-' and arr[1] != '*' and arr[1] != '/'):
+            raise Exception("Неверный оператор")
+        if len(arr) != 3:
+            raise Exception("Неверный ввод" )
 
-    if (num1 < 0 or num1 > 10):
-        raise Exception
-    if (num2 < 0 or num2 > 10):
-        raise Exception
-    if (arr[1] != '+' and arr[1] != '-' and arr[1] != '*' and arr[1] != '/'):
-        raise Exception
-    if len(arr) != 3:
-        raise Exception
+    except Exception as e:
+        return e
 
-except:
-    print("Исключение")
-
-else:
-    if arr[1] == "+":
-        print(num1 + num2)
-    elif arr[1] == "-":
-        print(num1 - num2)
-    elif arr[1] == "*":
-        print(num1 * num2)
-    elif arr[1] == "/":
-        print(int(num1 / num2))
+    else:
+        if arr[1] == "+":
+        elif arr[1] == "-":
+            return(num1 - num2)
+        elif arr[1] == "*":
+            return(num1 * num2)
+        elif arr[1] == "/":
+            return(int(num1 / num2))
+print(main(str))
